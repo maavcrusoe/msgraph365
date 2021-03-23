8297f20c9143cd5645fe3e0cd76cc6ec375b1868
Miquel Angel Aloy Vich <40128163+maavcrusoe@users.noreply.github.com>
2021-03-23 12:40:06 +0100

    Create Export-vCard.ps1

---
+++ Contacts/Export-vCard.ps1
@@ -0,0 +1,27 @@
+function Out-vCard {
+    $input | ForEach-Object {
+  
+    $vCardPath = "c:\tmp\vcard\" + $input + ".vcf"
+    Remove-Item $vCardPath -ErrorAction SilentlyContinue
+    
+    Add-Content -Path $vCardPath -Value "BEGIN:VCARD"
+    Add-Content -Path $vCardPath -Value "VERSION:2.1"
+    Add-Content -Path $vCardPath -Value "N:$($user.displayName)"
+    Add-Content -Path $vCardPath -Value "ORG:$($user.department)"
+    Add-Content -Path $vCardPath -Value "TITLE:$($user.jobTitle)"
+    Add-Content -Path $vCardPath -Value "TEL;WORK;VOICE:$($user.mobilePhone)"
+    Add-Content -Path $vCardPath -Value "TEL;HOME;VOICE:$($user.extension_xxx_ipPhone)"
+    Add-Content -Path $vCardPath -Value "TEL;CELL;VOICE:$($user.extension_xxx_pager)"
+    Add-Content -Path $vCardPath -Value "ADR;WORK;PREF:$($user.streetAddress)"
+    Add-Content -Path $vCardPath -Value "EMAIL;PREF;INTERNET:$($user.userPrincipalName)"
+    Add-Content -Path $vCardPath -Value "END:VCARD"
+    }
+}
+#list of users
+$users = @("user1@domain.com","user2@domain.com","user3@domain.com")
+
+foreach ($item in $users) {
+    #Function to obtain user data from MS Graph API
+    $user = GetUserData -userId $item
+    $u = $user.userPrincipalName | Out-vCard 
+}
