0a0d9442c12a244e30f6757052f1f8f84a22c3ca
Miquel Angel Aloy Vich <40128163+maavcrusoe@users.noreply.github.com>
2021-03-24 13:00:51 +0100

    Update Get-ContactsByUPN.ps1

--- Contacts/Get-ContactsByUPN.ps1
+++ Contacts/Get-ContactsByUPN.ps1
@@ -1,11 +1,10 @@
 function Get-ContactsByUPN {
     param ($userId)
-    
     $apiUrl = "https://graph.microsoft.com/v1.0/users/$userId/contacts" #test
     $Data = Invoke-RestMethod -Headers @{Authorization = "Bearer $($Tokenresponse.access_token)"} -Uri $apiUrl -Method Get
     $Contacts = ($Data | Select-Object Value).Value 
     #$Contacts | Out-GridView
-
+    $Report = [System.Collections.Generic.List[Object]]::new()
     foreach ($contact in $Contacts) {
         $idContact = $contact.id
         $displayName =  $contact.displayName
@@ -13,11 +12,29 @@ function Get-ContactsByUPN {
         $businessPhones =  $contact.businessPhones
         $homePhones =  $contact.homePhones
 
-        #write-host "id:" $idContact
-        #write-host "Name:" $displayName
-        #write-host "Mobile:" $mobilePhone         #largo tlf xxx xx xx xx
-        #write-host "ext movil:" $businessPhones   #extension movil xxx
-        #write-host "ext:" $homePhones             #extension fija xxxx
+        write-host "id:" $idContact -BackgroundColor Blue
+        write-host "Name:" $displayName -BackgroundColor Blue
+        write-host "Mobile:" $mobilePhone -BackgroundColor Blue   #largo tlf xxx xx xx xx
+        write-host "ext movil:" $businessPhones -BackgroundColor Blue  #extension movil xxx
+        write-host "ext:" $homePhones -BackgroundColor Blue       #extension fija xxxx
+
+        foreach ($User in $contact) {
+            $contactid = $User.id
+            $userdisplayName = $User.displayName
+            $userPrincipalName = $User.userPrincipalName
+            
+            #write-host $usersdisplayName,$usersmobile -ForegroundColor green
+            $ListContact = [PSCustomObject]@{
+                contactid    = $contactid
+                userdisplayName    = $userdisplayName
+                userPrincipalName    = $userPrincipalName
+            }      
+            $Report.Add($ListContact)     
+        }
     }
-    return $Contacts  
+    if (!$Report) {
+        write-host "Any contacts found on this user"
+    }
+    $Report | Out-GridView
+    return $Report
 }
