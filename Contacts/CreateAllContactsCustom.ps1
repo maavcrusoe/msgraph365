759725f14c67b7751a60de0fe7ffffa898c4a607
Miquel Angel Aloy Vich <40128163+maavcrusoe@users.noreply.github.com>
2021-03-25 12:31:54 +0100

    Create CreateAllContactsCustom.ps1

---
+++ Contacts/CreateAllContactsCustom.ps1
@@ -0,0 +1,31 @@
+function CreateAllContactsCustom {
+    $listUsers = @("user1@domain.com","user2@domain.com")
+    #call all users and store to var1
+    $var1 = Get-AllUsers 
+
+    foreach ($listUser in $listUsers) {
+        write-host "Creating contacts to: "$listUser -ForegroundColor Yellow
+        Start-sleep 5
+
+        foreach ($item in $var1.userPrincipalName) {
+            #Get user info like tlf number (xxx, xxxx, xxx xx xx xx), department...
+            $Data = GetUserData -userId $item
+            Write-Host "Add contact name: " $item -ForegroundColor green
+            
+            #Store data on var
+            $pager = $Data.extension_xxxx_pager
+            $ipPhone = $Data.extension_xxxx_ipPhone
+            $homePhone = $Data.extension_xxxx_homePhone
+            $mobilePhone = $Data.mobilePhone
+            $userPrincipalName = $Data.userPrincipalName
+            $displayName = $Data.displayName
+            $department = $Data.department
+            $jobTitle = $Data.jobTitle 
+    
+            CreateContact -userId $listUser -displayName $displayName -userPrincipalName $userPrincipalName -mobilePhone $mobilePhone -homePhone $homePhone -ipPhone $ipPhone -pager $pager -department $department -jobTitle $jobTitle        
+        }
+
+    }   
+
+}
+CreateAllContactsCustom
