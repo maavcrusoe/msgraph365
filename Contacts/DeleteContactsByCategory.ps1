56dfaede1b4baa292fea4b7674f051eac3525232
Miquel Angel Aloy Vich <40128163+maavcrusoe@users.noreply.github.com>
2021-04-16 08:14:27 +0200

    Create DeleteContactsByCategory.ps1

---
+++ Contacts/DeleteContactsByCategory.ps1
@@ -0,0 +1,25 @@
+function DeleteContactsByCategory {
+    param ($userId, $category )
+    $apiUrl = "https://graph.microsoft.com/v1.0/users/$userId/contacts?`$top=1000"
+    $Data = Invoke-RestMethod -Headers @{Authorization = "Bearer $($Tokenresponse.access_token)"} -Uri $apiUrl -Method "GET"
+    $Contacts = ($Data | Select-Object Value).Value 
+
+    foreach ($contact in $Contacts) {
+        $idContact = $contact.id
+        $displayName = $contact.displayName
+        $cat = $contact.categories
+        Write-Host $displayName -ForegroundColor Yellow
+        if ($cat -eq $category ) {
+            write-host "Contact found: " $displayName -ForegroundColor Green
+            Write-Host "Delete Contact" -ForegroundColor Red
+            $apiUrl = "https://graph.microsoft.com/beta/users/$userId/contacts/$idContact"
+            Invoke-RestMethod -Headers @{Authorization = "Bearer $($Tokenresponse.access_token)"} -Uri $apiUrl -Method "DELETE"
+        }elseif (!$idContact) {
+            write-host "Contact id not match" -ForegroundColor Red
+        }else{
+            Write-host "No contacts found"
+        }
+
+    }
+}
+#DeleteContactsByCategory -userId "alberto.ruiz@tunel.com" -category "business"
