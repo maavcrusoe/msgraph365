bf1e4791973ad05865b153c53d0db7c291f010f3
Miquel Angel Aloy Vich <40128163+maavcrusoe@users.noreply.github.com>
2021-05-25 11:23:53 +0200

    Update Create-Contact.ps1

--- Contacts/Create-Contact.ps1
+++ Contacts/Create-Contact.ps1
@@ -1,10 +1,11 @@
 function CreateContact {
-    param ($userId,$displayName,$userPrincipalName,$mobilePhone,$homePhone,$ipPhone,$pager,$department,$jobTitle)
+    param ($userId,$displayName,$userPrincipalName,$mobilePhone,$homePhone,$ipPhone,$pager,$department,$jobTitle,$category,$companyName)
 
     Write-Host "Create Contact" -ForegroundColor Green
-    $newcontact = "{'givenName': '"+$displayName+"' ,'emailAddresses': [{'address':  '"+$userPrincipalName+"','name': '"+$displayName+"'}], 'homePhones': ['"+$ipPhone+"','"+$homePhone+"'],'department': '"+$department+"','jobTitle': '"+$jobTitle+"', 'mobilePhone': '"+$mobilePhone+"','businessPhones': ['"+$pager+"']}"
+    $newcontact = "{'givenName': '"+$displayName+"' ,'emailAddresses': [{'address':  '"+$userPrincipalName+"','name': '"+$displayName+"'}], 'homePhones': ['"+$ipPhone+"','"+$homePhone+"'],'department': '"+$department+"','jobTitle': '"+$jobTitle+"', 'mobilePhone': '"+$mobilePhone+"','businessPhones': ['"+$pager+"'],'categories': ['"+$category+"'],'companyName': '"+$companyName+"'}" #,'otherAddress': {'street': '','city': '39.62540063774273','state': '2.7165319720767873','countryOrRegion': '','postalCode': ''}
     
     #write-host $newcontact
     $apiUrl = "https://graph.microsoft.com/v1.0/users/$userId/contacts"
     Invoke-RestMethod -Headers @{Authorization = "Bearer $($Tokenresponse.access_token)"} -Uri $apiUrl -Method "POST" -Body $newcontact -ContentType "application/json;charset=utf-8"
+    Write-Host "Contact created" -ForegroundColor Green
 }
