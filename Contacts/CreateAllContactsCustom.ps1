09368822061a0346363f6229519cb9ac6ea295d8
Miquel Angel Aloy Vich <40128163+maavcrusoe@users.noreply.github.com>
2021-05-25 11:22:45 +0200

    Update CreateAllContactsCustom.ps1

--- Contacts/CreateAllContactsCustom.ps1
+++ Contacts/CreateAllContactsCustom.ps1
@@ -21,10 +21,28 @@ function CreateAllContactsCustom {
             $displayName = $Data.displayName
             $department = $Data.department
             $jobTitle = $Data.jobTitle 
+            $companyName = $Data.companyName
     
-            CreateContact -userId $listUser -displayName $displayName -userPrincipalName $userPrincipalName -mobilePhone $mobilePhone -homePhone $homePhone -ipPhone $ipPhone -pager $pager -department $department -jobTitle $jobTitle        
+            CreateContact -userId $listUser -displayName $displayName -userPrincipalName $userPrincipalName -mobilePhone $pager -homePhone $homePhone -ipPhone $ipPhone -pager $mobilePhone -department $department -jobTitle $jobTitle -category $category -companyName $companyName        
         }
+    
+        #If you have OrgContacts
+        #Get user info like tlf number (xxx, xxxx, xxx xx xx xx), department...
+        $Data = GetOrgContacts
 
+        foreach ($item in $Data) {
+            #Store data on var
+            write-host "Add OrgContact: " $item.displayName -ForegroundColor green
+            $pager    = $item.pager
+            $mobile    = $item.mobile
+            $mail    = $item.mail
+            $companyName    = $item.companyName
+            $displayName    = $item.displayName
+            $department    = $item.department
+            
+            #write-host "add contacts to $listUser" -displayName $displayName -userPrincipalName $mail -mobilePhone $mobile -homePhone $homePhone -ipPhone $ipPhone -pager $pager -department $department -jobTitle $companyName        
+            CreateContact -userId $listUser -displayName $displayName -userPrincipalName $mail -mobilePhone $mobile -homePhone $homePhone -ipPhone $ipPhone -pager $pager -department $department -jobTitle $companyName               
+        }
     }   
 
 }
