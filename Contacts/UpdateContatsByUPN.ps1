bc432b9abbe559bbbb13f0e93595f9cefc59605a
Miquel Angel Aloy Vich <40128163+maavcrusoe@users.noreply.github.com>
2021-05-27 16:10:48 +0200

    Update UpdateContatsByUPN.ps1

--- Contacts/UpdateContatsByUPN.ps1
+++ Contacts/UpdateContatsByUPN.ps1
@@ -1,3 +1,41 @@
+function CreateNewContacts {
+    param($userId, $LocalContacts)
+    #call all users and store to var1
+    $var1 = Get-AllUsers 
+    $category = "empresa"
+    
+    foreach ($item in $var1.userPrincipalName) {
+        #filter by empresa tag
+        if ($LocalContacts.categories -eq "empresa") {
+            #if item is in local contacts then ignore
+            if ($item -in $LocalContacts.emailAddresses) {
+                write-host "Local Contact found" $item
+                
+            }else {
+                #else item isn't in local contacts then get user data and import it
+                ##Get user info like tlf number (xxx, xxxx, xxx xx xx xx), department...
+                $Data = GetUserData -userId $item
+                Write-Host "Add contact name: " $item -ForegroundColor green
+
+                #Store data on var
+                $pager = $Data.pager
+                $ipPhone = $Data.ipPhone
+                $homePhone = $Data.homePhone
+                $mobilePhone = $Data.mobilePhone
+                $userPrincipalName = $Data.userPrincipalName
+                $displayName = $Data.displayName
+                $department = $Data.department
+                $jobTitle = $Data.jobTitle 
+                $companyName = $Data.companyName
+                
+                #write-host "add contacts to $listUser" -displayName $displayName -userPrincipalName $userPrincipalName -mobilePhone $mobilePhone -homePhone $homePhone -ipPhone $ipPhone -pager $pager -department $department -jobTitle $jobTitle        
+                CreateContact -userId $userId -displayName $displayName -userPrincipalName $userPrincipalName -mobilePhone $pager -homePhone $homePhone -ipPhone $ipPhone -pager $mobilePhone -department $department -jobTitle $jobTitle -category $category -companyName $companyName        
+                }
+        }
+    }
+}
+
+
 function UpdateContactsByUPN {
     param ($userId)
     #call all users and store to var1
