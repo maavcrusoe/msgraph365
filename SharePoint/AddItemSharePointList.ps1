083abb252d36eee5523ab01c5a6dfb9fb88645ea
Miquel Angel Aloy Vich <40128163+maavcrusoe@users.noreply.github.com>
2021-06-29 15:58:53 +0200

    Update AddItemSharePointList.ps1

--- SharePoint/AddItemSharePointList.ps1
+++ SharePoint/AddItemSharePointList.ps1
@@ -1,10 +1,10 @@
 #insert into a SharePoint List
 #be carefull if you have a SPList in a Group check $url
 function AddItemSPO {
-  param ($site, $list, $idgroup, $value)
+  param ($site, $list, $name, $value)
   
   $url = "https://graph.microsoft.com/v1.0/sites/$site/lists/$list/items"
-  $newItem = "{ 'fields': {'Title': '"+$idgroup+"' ,'value': '"+$value+"'}}" 
+  $newItem = "{ 'fields': {'Title': '"+$name+"' ,'value': '"+$value+"'}}" 
   write-host $newItem -f 'g'
   
   Invoke-RestMethod -Headers @{Authorization = "Bearer $($Tokenresponse.access_token)"} -Uri $url -Method "POST" -Body $newItem -ContentType "application/json;charset=utf-8"
