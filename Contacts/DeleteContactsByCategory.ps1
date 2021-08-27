16f97606b072f82b8df5ea299a02103fedf844a1
Miquel Angel Aloy Vich <40128163+maavcrusoe@users.noreply.github.com>
2021-08-27 12:33:04 +0200

    Update DeleteContactsByCategory.ps1

--- Contacts/DeleteContactsByCategory.ps1
+++ Contacts/DeleteContactsByCategory.ps1
@@ -1,25 +1,29 @@
 function DeleteContactsByCategory {
     param ($userId, $category )
     $apiUrl = "https://graph.microsoft.com/v1.0/users/$userId/contacts?`$top=1000"
-    $Data = Invoke-RestMethod -Headers @{Authorization = "Bearer $($Tokenresponse.access_token)"} -Uri $apiUrl -Method "GET"
+    $Data = Invoke-RestMethod -Headers @{Authorization = "Bearer $($global:TokenResponse.access_token)"} -Uri $apiUrl -Method "GET"
     $Contacts = ($Data | Select-Object Value).Value 
-
-    foreach ($contact in $Contacts) {
+    Write-Host "Deleteing contacts to: "$userId -ForegroundColor Yellow
+    LogMessage -Message "Deleteing contacts to: " $userId
+   
+   foreach ($contact in $Contacts) {
         $idContact = $contact.id
         $displayName = $contact.displayName
         $cat = $contact.categories
-        Write-Host $displayName -ForegroundColor Yellow
-        if ($cat -eq $category ) {
-            write-host "Contact found: " $displayName -ForegroundColor Green
-            Write-Host "Delete Contact" -ForegroundColor Red
+        
+        if ($cat -ceq $category ) {
+            #write-host "Contact found: " $displayName -ForegroundColor Green
+            Write-Host "Delete Contact $displayName" -ForegroundColor Red
+            LogMessage -Message "Delete Contact: " $displayName
             $apiUrl = "https://graph.microsoft.com/beta/users/$userId/contacts/$idContact"
-            Invoke-RestMethod -Headers @{Authorization = "Bearer $($Tokenresponse.access_token)"} -Uri $apiUrl -Method "DELETE"
+            Invoke-RestMethod -Headers @{Authorization = "Bearer $($global:TokenResponse.access_token)"} -Uri $apiUrl -Method "DELETE"
+            
         }elseif (!$idContact) {
             write-host "Contact id not match" -ForegroundColor Red
         }else{
-            Write-host "No contacts found"
+            Write-host "No match category"
         }
-
     }
 }
+
 #DeleteContactsByCategory -userId "user@domain.com" -category "business"
