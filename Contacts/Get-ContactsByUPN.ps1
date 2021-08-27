36b6839ac767444f187e5228286c555ea8ed99e5
Miquel Angel Aloy Vich <40128163+maavcrusoe@users.noreply.github.com>
2021-08-27 12:20:36 +0200

    Update Get-ContactsByUPN.ps1
    added array of users to ignore if your need

--- Contacts/Get-ContactsByUPN.ps1
+++ Contacts/Get-ContactsByUPN.ps1
@@ -1,56 +1,76 @@
+$OnlyUserADlist =  @(
+    "user1@domain.com",
+    "user2@domain.com"
+    )
+
+
 function Get-ContactsByUPN {
-    param ($userId)
-    $apiUrl = "https://graph.microsoft.com/v1.0/users/$userId/contacts" #test
-    $Data = Invoke-RestMethod -Headers @{Authorization = "Bearer $($Tokenresponse.access_token)"} -Uri $apiUrl -Method Get
+    param ($userId,$showgrid)
+    #check execution time because MS Graph Token only lasts 1 hour
+    timeExecution
+    $apiUrl = "https://graph.microsoft.com/v1.0/users/$userId/contacts?`$top=1000" #test
+    
+    $Data = Invoke-RestMethod -Headers @{Authorization = "Bearer $($global:TokenResponse.access_token)"} -Uri $apiUrl -Method Get
     $Contacts = ($Data | Select-Object Value).Value 
+    write-host "Total local contacts: "$Contacts.Count -ForegroundColor Yellow
+    
+    #debug if your need
     #$Contacts | Out-GridView
-    $Report = [System.Collections.Generic.List[Object]]::new()
-    foreach ($contact in $Contacts) {
-        $idContact = $contact.id
-        $displayName =  $contact.displayName
-        $mobilePhone =  $contact.mobilePhone
-        $businessPhones =  $contact.businessPhones
-        $homePhones =  $contact.homePhones
-        $categories =  $contact.categories
-        $userPrincipalName = $contact.userPrincipalName
-        $emailAddresses = $contact.emailAddresses[0].address
-
-        write-host "id:" $idContact -BackgroundColor Blue
-        write-host "userPrincipalName:" $userPrincipalName -BackgroundColor Blue
-        write-host "emailAddresses:" $emailAddresses -BackgroundColor Blue
-        write-host "Name:" $displayName 
-        write-host "Mobile:" $mobilePhone  #largo tlf xxx xx xx xx
-        write-host "ext movil:" $businessPhones  #extension movil xxx
-        write-host "ext:" $homePhones        #extension fija xxxx
-        write-host "categories:" $categories
-
-        foreach ($User in $contact) {
-            $contactid = $User.id
-            $userdisplayName = $User.displayName
-            $userPrincipalName = $User.userPrincipalName
-            $businessPhones = $User.businessPhones
-            $mobilePhone = $User.mobilePhone
-            $homePhones = $User.homePhones
-            $categories = $User.categories
-            $emailAddresses = $User.emailAddresses
+    #$Data | Out-GridView
+    
+    #array of users to ignore
+    if ($OnlyUserADlist -contains $userId) {
+        #Write-Host "user in ignore list"
+    }else {
+        if ($Contacts.Count -eq 0) {
+            LogMessage -Message "NEW USER DETECTED CREATING CONTACTS:  $userId "
+            CreateAllContactsByUser -userId $userId
+        }
+        
+        $Report = [System.Collections.Generic.List[Object]]::new()
+        foreach ($contact in $Contacts) {
+            $idContact = $contact.id
+            $displayName =  $contact.displayName
+            $mobilePhone =  $contact.mobilePhone
+            $businessPhones =  $contact.businessPhones
+            $homePhones =  $contact.homePhones
+            $categories =  $contact.categories
+            $userPrincipalName = $contact.userPrincipalName
+            $emailAddresses = $contact.emailAddresses[0].address
             
-            #write-host $usersdisplayName,$usersmobile -ForegroundColor green
-            $ListContact = [PSCustomObject]@{
-                contactid    = $contactid
-                userdisplayName    = $userdisplayName
-                userPrincipalName    = $userPrincipalName
-                businessPhones    = $businessPhones
-                mobilePhone    = $mobilePhone
-                homePhones    = $homePhones
-                categories    = $categories
-                emailAddresses    = $emailAddresses
-            }      
-            $Report.Add($ListContact)     
+            foreach ($User in $contact) {
+                $contactid = $User.id
+                $userdisplayName = $User.displayName
+                $userPrincipalName = $User.userPrincipalName
+                $businessPhones = $User.businessPhones
+                $mobilePhone = $User.mobilePhone
+                $homePhones = $User.homePhones
+                $categories = $User.categories
+                $emailAddresses = $User.emailAddresses
+                
+                #write-host $usersdisplayName,$usersmobile -ForegroundColor green
+                $ListContact = [PSCustomObject]@{
+                    contactid    = $contactid
+                    userdisplayName    = $userdisplayName
+                    userPrincipalName    = $userPrincipalName
+                    businessPhones    = $businessPhones
+                    mobilePhone    = $mobilePhone
+                    homePhones    = $homePhones
+                    categories    = $categories
+                    emailAddresses    = $emailAddresses[0].address
+                }      
+                $Report.Add($ListContact)     
+            }
         }
     }
     if (!$Report) {
         write-host "Any contacts found on this user"
     }
-    $Report | Out-GridView
+
+    if ($showgrid -eq $true) {
+        $Report | Out-GridView   
+    }
+
     return $Report
+    
 }
