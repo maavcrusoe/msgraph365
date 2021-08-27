f0e4b6e16211531cbd3378ad79796c70639a6e00
Miquel Angel Aloy Vich <40128163+maavcrusoe@users.noreply.github.com>
2021-08-27 12:32:38 +0200

    Update DeleteContactsByID.ps1

--- Contacts/DeleteContactsByID.ps1
+++ Contacts/DeleteContactsByID.ps1
@@ -1,24 +1,27 @@
 function DeleteContactsByID {
     param ($userId, $contactId )
-    $apiUrl = "https://graph.microsoft.com/beta/users/$userId/contacts/"
-    $Data = Invoke-RestMethod -Headers @{Authorization = "Bearer $($Tokenresponse.access_token)"} -Uri $apiUrl -Method "GET"
+    $apiUrl = "https://graph.microsoft.com/beta/users/$userId/contacts?`$top=10000" #&skip=1000
+    $Data = Invoke-RestMethod -Headers @{Authorization = "Bearer $($global:TokenResponse.access_token)"} -Uri $apiUrl -Method "GET"
     $Contacts = ($Data | Select-Object Value).Value 
-    
+
     foreach ($contact in $Contacts) {
         $idContact = $contact.id
         $displayName = $contact.displayName
-
-        if ($contactId -eq $idContact ) {
+        
+        if ($contactId -ceq $idContact ) {
             write-host "Contact found: " $displayName -ForegroundColor Green
             Write-Host "Delete Contact" -ForegroundColor Red
+            LogMessage -Message "Contact deleted: " $displayName
             $apiUrl = "https://graph.microsoft.com/beta/users/$userId/contacts/$contactId"
-            Invoke-RestMethod -Headers @{Authorization = "Bearer $($Tokenresponse.access_token)"} -Uri $apiUrl -Method "DELETE"
+            Invoke-RestMethod -Headers @{Authorization = "Bearer $($global:TokenResponse.access_token)"} -Uri $apiUrl -Method "DELETE"
         }elseif (!$idContact) {
             write-host "Contact id not match" -ForegroundColor Red
         }else{
-            write-host "Contact not found"
+            #Write-host "No contacts found"
         }
+
     }
+
 }
 
 DeleteContactsByID -userId "user1@domain.com" -contactId "xxxx"
