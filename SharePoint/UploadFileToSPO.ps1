ec4d38ec9b433f0092610881a98e49c9c513cd45
Miquel Angel Aloy Vich <40128163+maavcrusoe@users.noreply.github.com>
2022-01-04 12:06:04 +0100

    Create UploadFileToSPO.ps1

---
+++ SharePoint/UploadFileToSPO.ps1
@@ -0,0 +1,55 @@
+function GetMSGraphToken {
+   #MSGRAPH
+    $clientID = "xxxxxxxx"
+    $tenantName = "xxxxxx.onmicrosoft.com"
+    $clientSecret = "xxxxxxxxxxxx"
+    $resource = "https://graph.microsoft.com/"
+
+    $ReqTokenBody = @{
+        Grant_Type    = "client_credentials"
+        Scope         = "https://graph.microsoft.com/.default"
+        client_Id     = $clientID
+        Client_Secret = $clientSecret
+    } 
+    $global:TokenResponse = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$TenantName/oauth2/v2.0/token" -Method POST -Body $ReqTokenBody
+    return $global:TokenResponse    
+}
+
+GetMSGraphToken
+
+function uploadFileSPO {
+    param ($fileInBytes, $filename, $remotePath)
+    $headers = @{
+        "Content-type"  = "application/json;charset=utf-8"
+        "Authorization" = "Bearer $($global:TokenResponse.access_token)"
+    }
+    
+    $url = "$($remotepath)$($filename):/content"
+    #put files on SPO
+    $response = Invoke-RestMethod -Method 'Put' -Uri $url -Body $fileInBytes -Headers $headers
+    
+    if (!$response.webUrl){
+        write-host "Problem uploading.." -ForegroundColor Red
+    }else {
+        Write-host "Upload completed!" -ForegroundColor Green
+        Write-host $response.webUrl -ForegroundColor Green
+    }
+}
+
+function GetFile {
+    param($fullPath)
+    #obtain file datainfo
+    $fileInBytes = [System.IO.File]::ReadAllBytes($fullPath)
+    $fileLength = $fileInBytes.Length
+
+    #write-host $fileInBytes
+    return $fileInBytes
+}
+
+$driveID = "xxxx"
+$fullPath = "xxxx"
+$remotePath = "https://graph.microsoft.com/v1.0/drives/$driveID/root:/AND/"
+$fileInBytes = GetFile -fullPath $fullPath
+$filename = Get-ChildItem -Path $fullPath -Name
+
+uploadFileSPO -fileInBytes $fileInBytes -filename $item -remotePath $remotePathSPO
